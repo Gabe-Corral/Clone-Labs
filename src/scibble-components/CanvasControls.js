f57dac2b35e3931ef3c2048b15e80c87cac59100
Gabe-Corral <78gabe.corral@gmail.com>
2021-07-02 15:36:27 -0500

    Added color options and undo/redo functions

---
+++ src/scibble-components/CanvasControls.js
@@ -0,0 +1,148 @@
+import React from 'react';
+import Pencil from './assets/pencil.png';
+import Erase from './assets/erase.png';
+import Size from './assets/size.png';
+import Black from './assets/black.png';
+import Brown from './assets/brown.png';
+import Blue from './assets/blue.png';
+import Yellow from './assets/yellow.png';
+import Green from './assets/green.png';
+import Red from './assets/red.png';
+import Undo from './assets/undo.png';
+import Redo from './assets/redo.png';
+
+const CanvasControls = (props) => {
+
+  return (
+    <div className="cavnas-controls">
+      <input
+      type="image"
+      src={Pencil}
+      alt="pencil"
+      width="20"
+      height="20"
+      id="pencil-img"
+      onClick={props.onDraw}
+      />
+      <input
+      type="image"
+      src={Erase}
+      alt="erase"
+      width="20"
+      height="20"
+      id="erase-img"
+      onClick={props.onErase}
+      />
+      <input
+      type="image"
+      src={Undo}
+      alt="undo"
+      width="20"
+      height="20"
+      id="undo-img"
+      onClick={props.undoLastOperation}
+      />
+      <input
+      type="image"
+      src={Redo}
+      alt="redo"
+      width="20"
+      height="20"
+      id="redo-img"
+      onClick={props.redoLastOperation}
+      />
+      <input
+      type="image"
+      src={Size}
+      alt="pencil-size"
+      width="8"
+      height="8"
+      id="size-img"
+      onClick={() => props.onChangeSize(2)}
+      />
+      <input
+      type="image"
+      src={Size}
+      alt="pencil-size"
+      width="12"
+      height="12"
+      id="size-img"
+      onClick={() => props.onChangeSize(5)}
+      />
+      <input
+      type="image"
+      src={Size}
+      alt="pencil-size"
+      width="20"
+      height="20"
+      id="size-img"
+      onClick={() => props.onChangeSize(15)}
+      />
+      <input
+      type="image"
+      src={Size}
+      alt="pencil-size"
+      width="30"
+      height="30"
+      id="size-img"
+      onClick={() => props.onChangeSize(25)}
+      />
+      <input
+      type="image"
+      src={Black}
+      alt="black"
+      width="30"
+      height="30"
+      id="black-img"
+      onClick={() => props.onColorChange('black')}
+      />
+      <input
+      type="image"
+      src={Brown}
+      alt="brown"
+      width="30"
+      height="30"
+      id="brown-img"
+      onClick={() => props.onColorChange('brown')}
+      />
+      <input
+      type="image"
+      src={Red}
+      alt="red"
+      width="30"
+      height="30"
+      id="red-img"
+      onClick={() => props.onColorChange('red')}
+      />
+      <input
+      type="image"
+      src={Green}
+      alt="green"
+      width="30"
+      height="30"
+      id="green-img"
+      onClick={() => props.onColorChange('green')}
+      />
+      <input
+      type="image"
+      src={Yellow}
+      alt="yellow"
+      width="30"
+      height="30"
+      id="yellow-img"
+      onClick={() => props.onColorChange('yellow')}
+      />
+      <input
+      type="image"
+      src={Blue}
+      alt="blue"
+      width="30"
+      height="30"
+      id="blue-img"
+      onClick={() => props.onColorChange('blue')}
+      />
+    </div>
+  )
+}
+
+export default CanvasControls;
--- src/scibble-components/ScibbleGame.js
+++ src/scibble-components/ScibbleGame.js
@@ -1,34 +1,47 @@
 import React, { useEffect, useState, useRef } from 'react';
+import CanvasControls from './CanvasControls';
 
 const ScibbleGame = (props) => {
   const canvasRef = useRef(null);
-  const contexRef = useRef(null);
+  const contextRef = useRef(null);
+  const [undoSteps, setUndoSteps] = useState({});
+  const [redoStep, setRedoStep] = useState({});
+  const [undo, setUndo] = useState(0);
+  const [redo, setRedo] = useState(0);
+  const [currentColor, setCurrentColor] = useState('black');
   const [isDrawing, setIsDrawing] = useState(false);
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    canvas.width = window.innerWidth * 2;
-    canvas.height = window.innerHeight * 2;
-    canvas.style.width = `${window.innerWidth}px`;
-    canvas.style.height = `${window.innerHeight}px`;
+    canvas.width = window.innerWidth * 2 -800;
+    canvas.height = window.innerHeight * 2 -800;
+    canvas.style.width = `${window.innerWidth-400}px`;
+    canvas.style.height = `${window.innerHeight-400}px`;
 
     const context = canvas.getContext("2d");
     context.scale(2, 2);
     context.lineCap = "round";
     context.strokeStyle = "black";
     context.lineWidth = 5;
-    contexRef.current = context;
+    contextRef.current = context;
   }, [])
 
   const startDrawing = ({nativeEvent}) => {
-    const {x, y} = nativeEvent;
-    contexRef.current.beginPath();
-    contexRef.current.moveTo(x, y);
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    const temp = {
+      ...undoSteps,
+      [undo + 1]: []
+    };
+    temp[undo + 1].push({ offsetX, offsetY });
+    setUndoSteps(temp);
+    setUndo(undo + 1);
     setIsDrawing(true);
   }
 
   const stopDrawing = () => {
-    contexRef.current.closePath();
+    contextRef.current.closePath();
     setIsDrawing(false);
   }
 
@@ -36,18 +49,117 @@ const ScibbleGame = (props) => {
     if (!isDrawing) {
       return;
     }
-    const {x, y} = nativeEvent;
-    contexRef.current.lineTo(x, y);
-    contexRef.current.stroke();
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+    const temp = {
+      ...undoSteps
+    };
+    temp[undo].push({ offsetX, offsetY });
+    setUndoSteps(temp);
+  }
+
+  const undoLastOperation = () => {
+    console.log(undo)
+    if (undo > 0) {
+      const data = undoSteps[undo];
+      contextRef.current.strokeStyle = "#DCDCDC";
+      contextRef.current.beginPath();
+      contextRef.current.lineWidth = 5;
+      contextRef.current.moveTo(data[0].offsetX, data[0].offsetY);
+      data.forEach((item, index) => {
+        if (index !== 0) {
+          contextRef.current.lineTo(item.offsetX, item.offsetY);
+          contextRef.current.stroke();
+        }
+      });
+      contextRef.current.closePath();
+      contextRef.current.strokeStyle = currentColor;
+      const temp = {
+        ...undoSteps,
+        [undo]: []
+      };
+      const te = {
+        ...redoStep,
+        [redo + 1]: [...data]
+      };
+      setUndo(undo - 1);
+      setRedo(redo + 1);
+      setRedoStep(te);
+      setUndoSteps(temp);
+    }
+  }
+
+  const redoLastOperation = () => {
+    if (redo > 0) {
+      const data = redoStep[redo];
+      contextRef.current.strokeStyle = "black";
+      contextRef.current.beginPath();
+      contextRef.current.lineWidth = 5;
+      contextRef.current.moveTo(data[0].offsetX, data[0].offsetY);
+      data.forEach((item, index) => {
+        if (index !== 0) {
+          contextRef.current.lineTo(item.offsetX, item.offsetY);
+          contextRef.current.stroke();
+        }
+      });
+      contextRef.current.closePath();
+      const temp = {
+        ...redoStep,
+        [redo]: []
+      };
+      setUndo(undo + 1);
+      setRedo(redo - 1);
+      setRedoStep(temp);
+      setUndoSteps({
+        ...undoSteps,
+        [undo + 1]: [...data]
+      });
+    }
+  }
+
+  const onErase = () => {
+    const context = canvasRef.current.getContext("2d");
+    context.strokeStyle = "#DCDCDC";
+    context.lineWidth = 15;
+    contextRef.current = context;
+  }
+
+  const onDraw = () => {
+    const context = canvasRef.current.getContext("2d");
+    context.strokeStyle = "black";
+    context.lineWidth = 5;
+    contextRef.current = context;
+  }
+
+  const onChangeSize = (size) => {
+    const context = canvasRef.current.getContext("2d");
+    context.lineWidth = size;
+    contextRef.current = context;
+  }
+
+  const onColorChange = (color) => {
+    const context = canvasRef.current.getContext("2d");
+    context.strokeStyle = color;
+    contextRef.current = context;
+    setCurrentColor(color);
   }
 
   return (
-    <div>
+    <div className="canvas-container">
       <canvas className="draw-canvas"
         onMouseDown={startDrawing}
         onMouseUp={stopDrawing}
         onMouseMove={draw}
         ref={canvasRef}
+      />
+      <CanvasControls
+      onDraw={onDraw}
+      onErase={onErase}
+      onChangeSize={onChangeSize}
+      onColorChange={onColorChange}
+      undoLastOperation={undoLastOperation}
+      redoLastOperation={redoLastOperation}
       />
     </div>
   )
